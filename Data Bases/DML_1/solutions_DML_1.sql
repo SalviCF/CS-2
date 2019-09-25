27971cacf8073d9551808c5142308ab1562432fe
Salvi <scarrillo@uma.es>
2019-09-25 17:57:42 +0200

    Some deletions

--- Data Bases/DML_1/solutions_DML_1.sql
+++ Data Bases/DML_1/solutions_DML_1.sql
@@ -1,7 +1,8 @@
--- RELACIÓN 1
--- select * from sol_R_E;
+-- Author: Salvi CF
 
--- 1. OK
+-- DML 1 solutions
+
+-- 1. 
 select nombre, apellido1, apellido2, departamento
 from profesores
 where departamento = 1;
@@ -14,26 +15,22 @@ where departamento != 3;
 -- 3.
 select nombre, apellido1, apellido2, email
 from profesores
-where email like '%lcc.uma.es'; -- hay que usar LIKE, = no vale...
+where email like '%lcc.uma.es';
 
 -- 4.
 select nombre, apellido1, apellido2,  email
 from alumnos
-where email is null; 
--- no puedo poner email = null, no aparece nadie que cumpla eso. Ninguna fila hace true ese predicado.
+where email is null;
 
 -- 5.
 select nombre, curso, teoricos, practicos, teoricos+practicos, (teoricos*100)/(teoricos+practicos) "% TEORICO", (practicos*100)/(teoricos+practicos) "% PRACTICOS"
 from asignaturas
 where curso = 3;
--- hay un fallo al meter los datos en la fila 2; deberían ser 8 créditos en total.
 
 -- otra forma:
 select nombre, creditos, round ((teoricos*100)/(teoricos+practicos), 2) as teor, round((practicos*100)/(teoricos+practicos), 2) as pract
 from asignaturas
 where curso = 3;
--- redondeo a 2 decimales
--- uso as en vez de ""
 
 -- 6.
 select alumno, calificacion
@@ -44,7 +41,6 @@ order by alumno;
 -- 7.
 select nombre, hombres, mujeres, hombres+mujeres
 from municipio;
--- ¿Cómo se haría para sumar la población de cada uno de los municipios para saber la población total de todos los municipios de España?
 
 -- USO DE FUNCIONES
 
@@ -63,7 +59,7 @@ where email is null
       and genero like 'FEM'; 
 
 
--- otra manera de hacerlo: usando concat de forma anidada (para concatenar más de dos strings)
+-- otra manera:
 
 select concat(concat(concat(concat(concat(concat('El alumno ', nombre), ' ' ), apellido1), ' '), apellido2), ' no dispone de correo') "Correos"
 from alumnos
@@ -78,7 +74,7 @@ from alumnos
 where email is null
       and genero like 'FEM'; 
 
--- otra forma de hacerlo es con la función decode
+-- otra forma:
 
 select decode('MASC',genero, 'El alumno ', 'La alumna ')||nombre||' '||apellido1||' '||apellido2|| ' no dispone de correo'
 from alumnos
@@ -88,21 +84,16 @@ where email is null;
 select nombre, apellido1, apellido2, antiguedad, antiguedad-to_date('01/01/1990', 'dd/mm/yyyy')
 from profesores
 where antiguedad-to_date('01/01/1990', 'dd/mm/yyyy') < 0;
--- paso a tipo fecha la fecha que me indican y calculo la diferencia en días entre esa fecha y las demás fechas de ingreso.
--- si la fecha2 es más reciente que fecha1, el resultado estará en negativo.
--- eso quiere decir que me interesan los resultados negativos ya que significa que el ingreso se produjo antes de 01/01/1990
 
--- otra forma de hacerlo: usando la función EXTRACT
+-- otra forma de hacerlo:
 select nombre, apellido1, apellido2, antiguedad, extract (year from antiguedad)
 from profesores
 where extract (year from antiguedad) < 1990;
--- esta solución es menos precisa ya que sólo indica el año
 
 -- 10.
 select nombre, apellido1, apellido2, fecha_nacimiento, trunc((months_between(sysdate, fecha_nacimiento))/12) as EDAD
 from profesores
 where trunc((months_between(sysdate, fecha_nacimiento))/12) < 30;
--- Consigo el número de años y lo trunco. Luego filtro para que salgan los que tienen menos de 30 años.
 
 -- 11.
 select upper(nombre), upper(apellido1), upper(apellido2), trunc(months_between(sysdate, antiguedad)/12/3) as trienios_docencia
@@ -131,11 +122,6 @@ select nombre, nvl(to_char(creditos), 'NO ASIGNADO'), caracter
 from asignaturas
 where caracter like 'O%'; --(también puedo usar 0% o O_)
 
--- RECORDATORIO
-select nombre from profesores where nombre like 'Ma%'; -- que empiece por Ma
-select nombre from profesores where nombre like '%io'; -- que acabe en io
-select nombre from profesores where nombre like '%ar%'; -- que contenga una ar
-
 -- otra forma
 
 select nombre, nvl(to_char(creditos), 'No asignado'), caracter
@@ -147,10 +133,6 @@ select dni, nombre, apellido1, apellido2, fecha_prim_matricula,  trunc(months_be
 from alumnos
 where (months_between(sysdate, fecha_prim_matricula)) < 2;
 
--- RECORDATORIO
-select nombre, departamento from profesores where departamento between 1 and 3;
--- El operador between saca los valores comprendidos entre los valores indicados, no que sólo saque esos dos valores indicados.
-
 -- 15.
 select nombre, fecha_nacimiento, fecha_prim_matricula, trunc(months_between(fecha_prim_matricula, fecha_nacimiento)/12) as años_prim_matricula
 from alumnos
@@ -160,8 +142,3 @@ where (months_between(fecha_prim_matricula, fecha_nacimiento)/12) < 18;
 select nombre, fecha_prim_matricula, to_char(fecha_prim_matricula, 'Day') as día_matriculación
 from alumnos
 where to_char(fecha_prim_matricula, 'Day') like 'Lunes%';
--- mucho cuidado con esto, to_char(fecha_prim_matricula, 'Day') saca el día con un espacio después.
--- por tanto, hay que poner like 'Lunes%'
-
-
-
--- Data Bases/DML_2/solutions_DML_2.sql
+++ Data Bases/DML_2/solutions_DML_2.sql
@@ -1,13 +1,15 @@
- -- RELACIÓN 2 Funciones, Reuniones y Operaciones de Conjuntos
--- select * from sol_R_E;
+-- Author: Salvi CF
+
+-- DML 2 solutions: Funciones, Reuniones y Operaciones de Conjuntos
+
 
 -------------------- Reunión de tablas------------------------------------------------------------------------------------
 -- Ejercicio 1
 select p.nombre, p.apellido1, p.apellido2, d.nombre
 from profesores p, departamentos d
-where p.departamento = d.codigo             -- Asignación de cada profesor con su departamento
-      and d.nombre like 'Lenguaje%';        -- Además, que el nombre del departamento empiece por Lenguaje
-      
+where p.departamento = d.codigo             
+      and d.nombre like 'Lenguaje%';        
+
 -- Ejercicio 2
 select a.nombre, a.apellido1, a.apellido2, m.asignatura, asig.nombre, m.curso, nvl(to_char(asig.practicos), 'No tiene')
 from alumnos a, matricular m, asignaturas asig
@@ -17,8 +19,8 @@ where a.dni = m.alumno
       and upper(a.apellido2) = 'ALBA'
       and asig.codigo = m.asignatura;
  
--- Otra manera de hacerlo (la veré más adelante)
-select asig.codigo, asig.nombre, nvl(to_char(practicos), 'No tiene') PRACTICOS, mat.curso -- tengo que pasarlo a char porque son números...
+-- Otra manera de hacerlo:
+select asig.codigo, asig.nombre, nvl(to_char(practicos), 'No tiene') PRACTICOS, mat.curso 
 from asignaturas asig join matricular mat on(asig.codigo = mat.asignatura)
                       join alumnos alum on(mat.alumno = alum.dni)
 where alum.nombre = 'Nicolas' 
@@ -72,7 +74,7 @@ where p1.departamento = p2.departamento
       and p1.id < p2.id
       and trunc(abs(months_between(p1.antiguedad, p2.antiguedad))/12) < 2;
       
--- Ejercicio 9 (la solución debe estar mal, preguntar al profesor)
+-- Ejercicio 9 
 select a1.nombre, m1.calificacion, a2.nombre, m2.calificacion
 from alumnos a1, alumnos a2, matricular m1, matricular m2
 where a1.genero = 'MASC'
@@ -83,7 +85,7 @@ where a1.genero = 'MASC'
       and m2.asignatura = 112
       and to_char(a1.fecha_prim_matricula, 'ww') = to_char(a2.fecha_prim_matricula, 'ww');
       
--- Ejercicio 9 (preguntar al profesor)-----------------------------------------
+-- Ejercicio 9 
 select a1.nombre as EL, m1.calificacion, a2.nombre as ELLA, m2.calificacion
 from alumnos a1, alumnos a2, matricular m1, matricular m2
 where a1.genero = 'MASC'
@@ -111,8 +113,8 @@ where a1.genero = 'MASC'
       and m2.asignatura = 112
       and to_char(a1.fecha_prim_matricula, 'ww') = to_char(a2.fecha_prim_matricula, 'ww')
       and decode(m1.calificacion,'MH',10,'SB',9,'NT',7,'AP',5,'SP',2,0) < decode(m2.calificacion,'MH',10,'SB',9,'NT',7,'AP',5,'SP',2,0);
-      
-select * from sol_2_9;
+     
+
 -- Ejercicio 10
 select a1.nombre, a2.nombre, a3.nombre, a1.cod_materia, a2.cod_materia, a3.cod_materia
 from asignaturas a1, asignaturas a2, asignaturas a3
@@ -158,13 +160,12 @@ where alu.dni = mat.alumno
       and pro.apellido1 = 'Soler'
 order by alu.apellido1, apellido2, nombre;
 
--- con el natural join reúno las dos tablas con los atributos que se llamen igual (asignatura, grupo, curso)
 
 -- Ejercicio 13-------------------------------------------------------------------
 select alu.nombre, alu.apellido1, alu.apellido2
 from profesores pro, departamentos dep, impartir imp, alumnos alu, matricular mat
 where pro.departamento = dep.codigo
-      and dep.nombre = 'Lenguajes y Ciencias de la Computacion'        -- sin acento en computación
+      and dep.nombre = 'Lenguajes y Ciencias de la Computacion'        
       and imp.profesor = pro.id
       and alu.dni = mat.alumno
       and mat.asignatura = imp.asignatura
@@ -172,7 +173,7 @@ where pro.departamento = dep.codigo
       and mat.curso = imp.curso
 order by alu.apellido1;
 
--- otra manera
+-- otra manera:
 select distinct alu.nombre, alu.apellido1, alu.apellido2
 from profesores pro, departamentos dep, alumnos alu, matricular mat natural join impartir imp
 where pro.departamento = dep.codigo
@@ -182,8 +183,6 @@ where pro.departamento = dep.codigo
 order by alu.apellido1;
 
 -- Ejercicio 14 ------------------------------------------------------------------
--- La tabla materias no aparece en el modelo pero sí en las diapos. Se llama "materias"
--- El solucionario está mal proque no aparece Ana Jiménez
 
 select asig.nombre as Asignatura, mat.nombre as Materia, pro.nombre||' '||pro.apellido1||' '||pro.apellido2 as Profesor, imp.carga_creditos "CARGA CRÉDITOS"
 from profesores pro, impartir imp, asignaturas asig, materias mat
@@ -193,8 +192,6 @@ where pro.id = imp.profesor
       and imp.carga_creditos is not null
 order by mat.codigo, asig.nombre desc   
 ;
-
-select * from impartir;
 
 -- Ejercicio 15 ---------------------------------------------------------------------
 select asig.nombre "Asignatura", dep.nombre "Departamento", asig.creditos "Créditos", round((asig.practicos*100)/asig.creditos, 2) "% Prácticos" 
@@ -225,7 +222,7 @@ from alumnos
 where email is  not null);
 
 -- Ejercicio 18 ------------------------------------------------------------------
---initcap!!
+
 ((select initcap(apellido1) from profesores where apellido1 is not null)
 intersect
 (select initcap(apellido1) from alumnos where apellido1 is not null))
@@ -279,7 +276,6 @@ UNION
 (select replace(apellido2, 'll', 'y')
 from alumnos where upper(apellido2) like '%LL%');
 
-
 -- Solución  real
 (select replace(apellido1, 'll', 'y')
 from profesores where upper(apellido1) like '%LL%')
@@ -322,12 +318,6 @@ from alumnos where upper(apellido2) like '%LL%'));
 
 -- Reuniones externas: select ... from ... a (left/right) outer join b on () where ...
 -- Ejercicio 21-----------------------------------------------------------------------
--- Las asignaturas que tienen algún campo de credito como null no aparecen con la expresión asig.teoricos + asig.practicos != asig.creditos
--- algo + null = null
--- null + null = null (eso no sería una incongruencia) por eso Administración de bases de datos no debe aparecer el listado
--- la incongruencia llega cuando tengo algo + null = algo (Programación Orientada a Objetos)
--- Por tanto, si uno de los dos operandos es null y el resultado no es null, debo meter la asginatura en el listado de incongruencias
--- Uso left porque tengo que mostrar todas las asignaturas a las que le pase eso, aunque no tengan profesor asociado (Estadística)
 
 select asig.nombre, imp.profesor
 from asignaturas asig left outer join impartir imp on(asig.codigo = imp.asignatura)
@@ -350,7 +340,7 @@ from alumnos alu1 left outer join alumnos alu2 on(alu1.fecha_prim_matricula = al
                                                     and alu1.dni != alu2.dni)
 order by alu1.apellido1;
 
--- Resultado correcto a mi juicio
+-- Resultado correcto:
 select alu1.nombre, alu1.apellido1, alu1.apellido2, alu2.nombre, alu2.apellido1, alu2.apellido2, alu1.fecha_prim_matricula, alu2.fecha_prim_matricula
 from alumnos alu1 left outer join alumnos alu2 on(alu1.fecha_prim_matricula like alu2.fecha_prim_matricula
                                                     and alu1.dni != alu2.dni)
@@ -365,27 +355,24 @@ order by asig.nombre;
 -- Subconsultas------------------------------------------------------------------
 
 -- Ejercicio 26 -------------------------------------------------------------------
--- Es una consulta negativa (la pista la da el enunciado)
--- La expresión es cierta si el elemento  NO pertenece al conjunto
--- Se mostrarán los atributos del select siempre que el id del profesor no esté en el conjunto especificado por la subconsulta
 
 select nombre, apellido1, id
 from profesores pro
 where id not in (select profesor
-                  from impartir imp); -- los profesores que imparten
+                  from impartir imp); 
                   
 select nombre, apellido1, id
 from profesores pro
 where not exists (select profesor
                   from impartir imp
-                  where pro.id = imp.profesor); -- profesores que no imparten hacen la tupla vacía y lo selecciono para mostrarlo
+                  where pro.id = imp.profesor);
 
 -- Ejercicio 27 --------------------------------------------------------------------
 select nombre, apellido1, apellido2
 from alumnos
 where dni in (select alumno from matricular
               where asignatura = 115
-              and genero = 'FEM') -- dni de las alumnas matriculadas en la asignatura 115 (atributo género es de alumnos, no hay confusión)
+              and genero = 'FEM') 
       and rownum < 3;
       
 select nombre, apellido1, apellido2
@@ -393,7 +380,7 @@ from alumnos al
 where exists (select * from matricular mat
               where asignatura = 115
               and genero = 'FEM'
-              and al.dni = mat.alumno) -- 
+              and al.dni = mat.alumno) 
       and rownum < 3;
       
 -- Ejercicio 28 ------------------------------------------------------------------
@@ -401,14 +388,14 @@ select *
 from profesores
 where id not in (select director_tesis
                   from profesores
-                  where director_tesis is not null); -- id de directores de tesis. 
-                                                -- Tengo que poner que no sea null porque todos los profesores pertenecen aunque sea con valor null
+                  where director_tesis is not null); 
+                                                
                   
 select *
 from profesores p1
 where not exists (select *
                   from profesores p2
-                  where p1.id = p2.director_tesis); -- si p1 no es director de tesis de p2, tupla vacía y p1 se añade al resultado
+                  where p1.id = p2.director_tesis); 
                   
 -- Ejercicio 29 -----------------------------------------------------------------
 select nombre, codigo 
@@ -427,17 +414,3 @@ from asignaturas a1
 where a1.creditos < any(select creditos
                       from asignaturas a2
                       where a1.curso = a2.curso));
-                      
-                      select * from sol_2_30;
-                      
-select nombre, creditos, curso from asignaturas order by curso;
-
-
-
-
-
-
-
-
-
-
